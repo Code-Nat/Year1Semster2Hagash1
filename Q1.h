646aaa67ced731839c466f80e946a53837049458
Code-Nat <Nat@creative-shape.com>
2019-04-01 13:07:44 +0300

    Deleted unneeded files

--- Q1.h
+++
@@ -1,7 +0,0 @@
-
-int findDigit(int num, int place)
-{
-	while (place-- && num != 0)
-		num /= 10;
-	return num % 10;
-}
--- Q3.h
+++
@@ -1,9 +0,0 @@
-
-int sumOfDiv(int data[], int size, int div)
-{
-	int i, sum = 0;
-	for (i = 0; i < size; i++)
-		if (!(data[i] % div))
-			sum += data[i];
-	return sum;
-}
